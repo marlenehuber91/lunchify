4166acb2369fabd1e85673cce93b53de0493d8d8
12219457 <k12219457@jku.students.at>
2025-04-11 20:13:02 +0200

    Added ENUM FLAGGED in ReimbursementState

--- src/main/java/backend/model/ReimbursementState.java
+++ src/main/java/backend/model/ReimbursementState.java
@@ -3,7 +3,8 @@ package backend.model;
 public enum ReimbursementState {
     PENDING,       // Rechnung wurde hochgeladen, aber noch nicht verarbeitet
     APPROVED,      // Rechnung wurde geprüft und genehmigt
-    REJECTED;     // Rechnung wurde abgelehnt
+    REJECTED,      // Rechnung wurde abgelehnt
+    FLAGGED;	   // Rechnung muss von einem Admin geprüft werden, da Anomalie erkannt wurde
 
 
     // Optional: Methode zur Beschreibung des Status
@@ -15,6 +16,8 @@ public enum ReimbursementState {
                 return "Die Rechnung wurde genehmigt.";
             case REJECTED:
                 return "Die Rechnung wurde abgelehnt.";
+            case FLAGGED:
+            	return "Die Rechnung muss von einem Admin geprüft werden";
             default:
                 return "Unbekannter Status.";
         }
