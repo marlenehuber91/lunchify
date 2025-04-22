021ff10c7c7d4518a1f2767073654140f22efcec
marlenehuber91 <marlenehu@outlook.com>
2025-04-22 18:07:31 +0200

    fxml file done

---
+++ src/main/java/frontend/controller/SearchController.java
@@ -0,0 +1,16 @@
+package frontend.controller;
+
+import javafx.fxml.FXML;
+
+public class SearchController {
+
+    @FXML
+    void handleBackToDashboard() {
+
+    }
+
+    @FXML
+    void startSearchManually() {
+
+    }
+}
