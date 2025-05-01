9b0763d0af6a98375651305aed089df54d21e46e
SilviaMahr <s.mahringer@gmx.at>
2025-05-01 17:04:22 +0200

    #27 without Testing

--- src/main/java/frontend/controller/CurrReimbursementController.java
+++ src/main/java/frontend/controller/CurrReimbursementController.java
@@ -99,7 +99,7 @@ public class CurrReimbursementController {
 	public void handleUserChoice() {
 		Reimbursement selectedReimbursement = currReimbursementTable.getSelectionModel().getSelectedItem();
 		
-		if (selectedReimbursement != null) {
+		if (selectedReimbursement != null && selectedReimbursement.isReimbursementUserEditable(user.getId())) {
 
             if (user != null) {
                 try {
