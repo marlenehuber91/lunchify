fcdd583df80d33ac2efef653327809a0b98ed47a
12219457 <k12219457@jku.students.at>
2025-06-11 10:19:09 +0200

    removed 2 unused

--- src/main/java/frontend/controller/AdminEditReimbursementController.java
+++ src/main/java/frontend/controller/AdminEditReimbursementController.java
@@ -3,10 +3,8 @@ package frontend.controller;
 
 import backend.model.ReimbursementState;
 import javafx.collections.FXCollections;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
